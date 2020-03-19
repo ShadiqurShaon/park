d13fac078cfbbf14843fd6375c92ab9329180ca3
shadiqurshaon <shadiqurshaon@gmail.com>
2020-03-19 16:33:28 +0600

    add hour perquite file

--- park.py
+++ park.py
@@ -31,15 +31,31 @@ for index,item in date.iterrows():
     date_name = directory
     all_ochurance_in_a_day = 'select * from df where date = "'+date_name+'" limit 10'
     date_all = ps.sqldf(all_ochurance_in_a_day, globals())
-    # print(date_all)
 
+    all_distinct_hour = 'select distinct(time) from date_all'
+
+    distinct_time_of_a_day = ps.sqldf(all_distinct_hour, globals())
+
+    for index,val in distinct_time_of_a_day.iterrows():
+        inside_dir = val['time']
+        inside_path = path+'/'
+        time_path = os.path.join(inside_path,inside_dir) 
+        os.mkdir(time_path)
+        all_occurance_in_a_hour = "select * from date_all where time = '"+inside_dir+"'"
+        all_data_of_an_hour = ps.sqldf(all_occurance_in_a_hour, globals())
+        
+        put_per = time_path+'/'+inside_dir+'.parquet.gzip'
+        all_data_of_an_hour.to_parquet(put_per,compression='gzip') 
+        # print(all_data_of_an_hour)
+        # break
     #for csv
     # put_file = path+'/'+date_name+".csv"
     #for perquite
-    put_per = path+'/'+date_name+'.parquet.gzip'
     # print(put_file)
     # date_all.to_csv(put_file,index = False,header = True)
-    date_all.to_parquet(put_per,compression='gzip') 
+    # put_per = time_path+'/'+inside_dir+'.parquet.gzip'
+
+    # date_all.to_parquet(put_per,compression='gzip') 
 
 print("ok")
 # print(df.head())  
